2b29dbc95666c9074af03ba3cedcf0c5536524d6
santk97 <santk97@gmail.com>
2017-09-11 09:50:00 +0530

    udated

--- Change/hackathon/forms.py
+++ Change/hackathon/forms.py
@@ -1,4 +1,4 @@
-from models import UserModel,project_model
+from models import UserModel,project_model , indexmodel
 from django import forms
 
 class SignUpForm(forms.ModelForm):
@@ -7,7 +7,10 @@ class SignUpForm(forms.ModelForm):
     fields=['email','name','password','re_password']
 
 
-
+class Indexform1(forms.ModelForm):
+    class Meta:
+        model=indexmodel
+        fields=['first_name','last_name']
 
 class LoginForm(forms.ModelForm):
     class Meta:
