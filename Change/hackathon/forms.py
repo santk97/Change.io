b1c5898a17a5e51f86e2be3df7a29f146c7bef9d
vaidishan9@gmail.com <vaidishan9@gmail.com>
2017-09-11 15:39:04 +0530

    swachh

--- Change/hackathon/forms.py
+++ Change/hackathon/forms.py
@@ -1,10 +1,10 @@
 from models import UserModel,project_model , indexmodel,startmodel
-from models import UserModel,project_model , LikeModel,indexmodel ,swatch_UserModel
+from models import UserModel,project_model , PostModel,indexmodel ,swatch_UserModel
 
 from models import UserModel,project_model , indexmodel ,swatch_UserModel , feedback_model
 
 
-from models import UserModel,project_model , indexmodel ,swatch_UserModel , feedback_model
+from models import UserModel,project_model , indexmodel ,swatch_UserModel , feedback_model,CommentModel
 
 
 from django import forms
@@ -26,6 +26,10 @@ class LoginForm(forms.ModelForm):
       fields = ['email', 'password']
 
 
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = PostModel
+        fields = ['image', 'caption']
 class Startform(forms.ModelForm):
     class Meta:
         mode=startmodel
@@ -41,10 +45,10 @@ class swatch_LoginForm(forms.ModelForm):
       model = swatch_UserModel
       fields = ['email', 'password']
 
-class LikeForm(forms.ModelForm):
+class CommentForm(forms.ModelForm):
   class Meta:
-    model = LikeModel
-    fields = ['post']
+    model = CommentModel
+    fields = ['comment_text', 'post']
 class feedback_form(forms.ModelForm):
     class Meta:
         model=feedback_model
